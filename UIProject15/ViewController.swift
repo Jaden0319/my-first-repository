216986bee203ab206b1e15e274b711a226ef5c4f
Jaden Creech <jadencreech@icloud.com>
2022-08-08 17:51:21 -0400

    Added a smile

--- UIProject15/ViewController.swift
+++ UIProject15/ViewController.swift
@@ -9,9 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    public var smileLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemPink
+        
+        smileLabel = UILabel()
+        smileLabel.text = ":D"
+        smileLabel.textColor = .green
+        smileLabel.translatesAutoresizingMaskIntoConstraints = false
+        smileLabel.font = .boldSystemFont(ofSize: 25)
+        view.addSubview(smileLabel)
+        
+        setConstraints()
+    }
+    
+    public func setConstraints() {
+        
+        NSLayoutConstraint.activate([
+            
+            smileLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            smileLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
     }
 
 
